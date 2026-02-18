e3248dd5423e834cc53d9231aa3929314527fde0
Anadulce57 <andl2008@hotmail.com>
2026-02-18 10:17:04 -0300

    Sistema base feito com sucesso

--- main.cpp
+++ main.cpp
@@ -14,6 +14,7 @@ void adicionar(std::vector<livro>& listaLivros) {
     livro novo;
     char resposta;
 
+    std::cin.ignore();
     std::cout << "Titulo: ";
     std::getline(std::cin, novo.titulo);
 
@@ -23,7 +24,7 @@ void adicionar(std::vector<livro>& listaLivros) {
     std::cout << "Paginas: ";
     std::getline(std::cin, novo.paginas);
 
-    std:: cout << "Lido? (s/n) ";
+    std:: cout << "Lido? (s/n)";
     std:: cin >> resposta;
 
     if (resposta == 's'||resposta == 'S') {
@@ -37,7 +38,7 @@ void adicionar(std::vector<livro>& listaLivros) {
 }
 
 //Função de READ
-void listar(std::vector<livro>& lista) {
+void listar(const std::vector<livro>& lista) {
     std::cout << "Minha biblioteca" << std::endl;
     for (int i = 0; i < lista.size(); i++) {
         std::cout << i + 1 << ". " << lista[i].titulo << "-" << lista[i].autor << "-" << lista[i].paginas << std::endl;
@@ -49,16 +50,60 @@ void listar(std::vector<livro>& lista) {
 }
 
 //Função de UPDATE
+void atualizar(std::vector<livro>& lista) {
+    int indice;
+    std:: cout << "Qual é o livro lido? (índice)" << std::endl;
+    std:: cin >> indice;
+
+    lista[indice].lido = true;
+    std::cout << "Atualizado com sucesso!" << std::endl;
+}
 
 //Função de DELETE
+void deletar(std::vector<livro>& lista) { //removendo a partir do índice
+    int indice;
+    std::cout << "Digite o índice: " << std::endl;
+    std::cin >> indice;
+    lista.erase(lista.begin() + (indice-1));
 
+    std::cout << "Livro removido!" << std::endl;
+}
 
 
 int main() {
     std::vector<livro> biblioteca;
+    std::cout << "Bem vindo a um pequeno sistema 'CRUD' de biblioteca!" << std::endl;
+    std::cout << "Vamos comecar?" << std::endl;
 
+    int escolha;
+    do {
+        std::cout << "O que voce gostaria de fazer" << std::endl;
+        std::cout << "1 - Registrar\n";
+        std::cout << "2 - Listar\n";
+        std::cout << "3 - Deletar\n";
+        std::cout << "4 - Atualizar\n";
+        std::cout << "0 - Sair" << std::endl;
+        std::cin >> escolha;
+        switch (escolha) {
+            case 1:
+                adicionar(biblioteca);
+                break;
+            case 2:
+                listar(biblioteca);
+                break;
+            case 3:
+                deletar(biblioteca);
+                break;
+            case 4:
+                atualizar(biblioteca);
+                break;
+            case 0:
+                std::cout << "Saindo..." << std::endl;
+                break;
+            default:
+                std::cout << "Erro!" << std::endl;
+        }
+    } while (escolha != 0);
 
-    adicionar(biblioteca);
-    listar(biblioteca);
     return 0;
 }
