460f9a36182f518f7ebfba497380fee97409bab3
Anadulce57 <andl2008@hotmail.com>
2026-03-19 11:03:59 -0300

    Funções de criação e iniciar banco

--- main.cpp
+++ main.cpp
@@ -1,110 +1,35 @@
-#include <iostream>
-#include <vector>
-#include <string>
-#iclude <drogon/drogon.h>
+#include <iostream> //Declarando bibliotecas
+#include <sqlite3.h> //Praticamente o express
+#include <drogon/drogon.h> //Praticamente o npm
 
-struct livro { //Iniciando estrutura -> como se fosse um objeto com key values
-    std::string titulo;
-    std::string autor;
-    std::string paginas;
-    bool lido;
-};
+const std::string DB_PATH = "biblioteca.DB";
 
-//Função de CREATE
-void adicionar(std::vector<livro>& listaLivros) {
-    livro novo;
-    char resposta;
-
-    std::cin.ignore();
-    std::cout << "Titulo: ";
-    std::getline(std::cin, novo.titulo);
-
-    std::cout << "Autor: ";
-    std::getline(std::cin, novo.autor);
-
-    std::cout << "Paginas: ";
-    std::getline(std::cin, novo.paginas);
-
-    std:: cout << "Lido? (s/n)";
-    std:: cin >> resposta;
-
-    if (resposta == 's'||resposta == 'S') {
-        novo.lido = true;
-    }else{
-        novo.lido = false;
+sqlite3* abrirBanco() { //Praticamente o getConection * ponteiro
+    sqlite3 *db;
+    if (sqlite3_open(DB_PATH.c_str(), &db) != SQLITE_OK) {
+        std::cerr <<  "Erro ao abrir o banco!" << sqlite3_errmsg(db) << std::endl;
+        return nullptr;
     }
-
-    listaLivros.push_back(novo);
-    std::cout << "Cadastrado com sucesso!"<< std::endl;
+    return db;
 }
 
-//Função de READ
-void listar(const std::vector<livro>& lista) {
-    std::cout << "Minha biblioteca" << std::endl;
-    for (int i = 0; i < lista.size(); i++) {
-        std::cout << i + 1 << ". " << lista[i].titulo << "-" << lista[i].autor << "-" << lista[i].paginas << std::endl;
+void iniciarBanco() {
+    sqlite3 *db = abrirBanco();
+    if (!db) return;
 
-        if (lista[i].lido == true) {
-            std::cout << "Lido" << std::endl;
-        }
-    }
+    const char* sql = R"(
+       CREATE TABLE IF NOT EXISTS livros (
+           id INTEGER PRIMARY KEY AUTOINCREMENT,
+           titulo TEXT NOT NULL,
+           autor TEXT NOT NULL,
+           paginas INTEGER,
+           lido BOOLEAN DEFAULT 0
+       );
+    )";
+
+    char* erro;
+    sqlite3_exec(db, sql, nullptr, nullptr, &erro);
+    sqlite3_close(db);
 }
 
-//Função de UPDATE
-void atualizar(std::vector<livro>& lista) {
-    int indice;
-    std:: cout << "Qual é o livro lido? (índice)" << std::endl;
-    std:: cin >> indice;
-
-    lista[indice].lido = true;
-    std::cout << "Atualizado com sucesso!" << std::endl;
-}
-
-//Função de DELETE
-void deletar(std::vector<livro>& lista) { //removendo a partir do índice
-    int indice;
-    std::cout << "Digite o índice: " << std::endl;
-    std::cin >> indice;
-    lista.erase(lista.begin() + (indice-1));
-
-    std::cout << "Livro removido!" << std::endl;
-}
-
-
-int main() {
-    std::vector<livro> biblioteca;
-    std::cout << "Bem vindo a um pequeno sistema 'CRUD' de biblioteca!" << std::endl;
-    std::cout << "Vamos comecar?" << std::endl;
-
-    int escolha;
-    do {
-        std::cout << "O que voce gostaria de fazer" << std::endl;
-        std::cout << "1 - Registrar\n";
-        std::cout << "2 - Listar\n";
-        std::cout << "3 - Deletar\n";
-        std::cout << "4 - Atualizar\n";
-        std::cout << "0 - Sair" << std::endl;
-        std::cin >> escolha;
-        switch (escolha) {
-            case 1:
-                adicionar(biblioteca);
-                break;
-            case 2:
-                listar(biblioteca);
-                break;
-            case 3:
-                deletar(biblioteca);
-                break;
-            case 4:
-                atualizar(biblioteca);
-                break;
-            case 0:
-                std::cout << "Saindo..." << std::endl;
-                break;
-            default:
-                std::cout << "Erro!" << std::endl;
-        }
-    } while (escolha != 0);
-
-    return 0;
-}
+int main() {}
