b271d5a458cd632e4f92eb51c1c34df48afc4353
Anadulce57 <andl2008@hotmail.com>
2026-03-24 10:58:01 -0300

    Get livros, um erro aparente

--- main.cpp
+++ main.cpp
@@ -14,7 +14,7 @@ sqlite3* abrirBanco() { //Praticamente o getConection * ponteiro
 }
 
 void iniciarBanco() {
-    sqlite3 *db = abrirBanco();
+    sqlite3* db = abrirBanco();
     if (!db) return;
 
     const char* sql = R"(
@@ -32,4 +32,36 @@ void iniciarBanco() {
     sqlite3_close(db);
 }
 
-int main() {}
+int main() {
+    iniciarBanco(); //garantir a existencia da tabela de dados
+    //GET livros - app.get('/livros', (req, res) => { ... })
+    drogon::app().registerHandler("/livros",// "callback" é o "res" do Express
+        [](const drogon::HttpRequestPtr& req,
+            std::function<void(const drogon::HttpRequestPtr&)>&& __callback) { // "callback" é o "res" do Express
+            sqlite3* db = abrirBanco();
+            Json::Value resultado(Json::arrayValue); // equivalente ao "const lista = []"
+
+            if (db) {
+                sqlite3_stmt* stmt; // stmt = a query compilada, como um "prepared statement"
+                const char* sql = "SELECT id, titulo, autor, paginas, lido FROM livros;";
+
+                if (sqlite3_prepare_v2(db, sql, -1, &stmt, nullptr) == SQLITE_OK) { // Passo 1: compilar o SQL
+                    while (sqlite3_step(stmt) == SQLITE_ROW) {  // Passo 2: iterar pelas linhas - forEach()
+                        Json::Value livro;
+                        livro["id"] = sqlite3_column_int(stmt, 0);
+                        livro["titulo"] = reinterpret_cast<const char*>(sqlite3_column_int(stmt,1));
+                        livro["autor"] = reinterpret_cast<const char*>(sqlite3_column_int(stmt,2));
+                        livro["paginas"] = sqlite3_column_int(stmt, 3);
+                        livro["lido"] = sqlite3_column_int(stmt, 4) == 1;
+                        resultado.append(livro);
+                    }
+                    sqlite3_finalize(stmt); //libera memória da query
+                }
+                sqlite3_close(db); // fecha conexão
+            }
+            auto resp = drogon::HttpResponse::newHttpJsonResponse(resultado); // Monta e envia a resposta JSON
+            __callback(resp);
+        },
+        {drogon::Get}
+           );
+}
