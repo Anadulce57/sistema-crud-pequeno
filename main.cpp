836d7ec98f0d2bfddee6a8fea983c6037e8f95d4
Anadulce57 <andl2008@hotmail.com>
2026-07-04 23:43:20 -0300

    Segunda parte + teentativa de rodar

--- main.cpp
+++ main.cpp
@@ -34,7 +34,7 @@ void iniciarBanco() {
 
 int main() {
     iniciarBanco(); //garantir a existencia da tabela de dados
-    //GET livros - app.get('/livros', (req, res) => { ... })
+    //GET livros - app.get('/livros', (req, res) => { ... }) : para confirmar que aparece na lista
     drogon::app().registerHandler("/livros",// "callback" é o "res" do Express
         [](const drogon::HttpRequestPtr& req,
             std::function<void(const drogon::HttpRequestPtr&)>&& __callback) { // "callback" é o "res" do Express
@@ -64,4 +64,57 @@ int main() {
         },
         {drogon::Get}
            );
+
+    //POST /livros — equivalente ao app.post('/livros', (req, res) => { ... }) : para adicionar um livro
+    drogon::app().registerHandler("/livros",
+        [](const drogon::HttpRequestPtr& req,
+            std::function<void(const drogon::HttpResponsePtr&)>&& __callback) {
+
+                // Lê o body JSON — equivalente ao "req.body" do Express (com express.json())
+                auto body = req->getJsonObject();
+                Json::Value resposta;
+
+            // Verificação — equivalente ao "if (!req.body.titulo) return res.status(400)..."
+                if (!body || !(*body).isMember("titulo") || !(*body).isMember("autor")) {
+                    resposta["erro"] = "Campos 'título' e 'autor' são obrigatórios";
+                    auto resp = drogon::HttpResponse::newHttpJsonResponse(resposta);
+                    resp-> setStatusCode(drogon::k400BadRequest); //res.status(400)
+                    __callback(resp);
+                    return; // para a execução — equivalente ao "return res.status(400).json()"
+                }
+            // Extrai os valores do body — equivalente ao "const { titulo, autor } = req.body"
+            std::string titulo = (*body)["titulo"].asString();
+            std::string autor = (*body)["autor"].asString();
+            int paginas = (*body).get("paginas",0).asInt();
+
+            sqlite3* db = abrirBanco();
+            if (db) {
+                sqlite3_stmt* stmt;
+                //? como se fosse o placeholder
+                const char* sql = "INSERT INTO livros (titulo, autor, paginas, lido) VALUES (?, ?, ?, 0);";
+
+                if (sqlite3_prepare_v2(db, sql, -1, &stmt, nullptr) == SQLITE_OK) {
+                    // Bind: preenche os "?" em ordem — equivalente ao array de params do pg
+                    sqlite3_bind_text(stmt, 1, titulo.c_str(), -1, SQLITE_STATIC);
+                    sqlite3_bind_text(stmt, 2, autor.c_str(), -1, SQLITE_STATIC);
+                    sqlite3_bind_int(stmt, 3, paginas);
+                    sqlite3_step(stmt); //executar insert
+                    sqlite3_finalize(stmt); //liberar memória
+                }
+                sqlite3_close(db);
+            }
+            resposta["mensagem"] = "Livro cadastrado com sucesso!";
+            auto resp = drogon::HttpResponse::newHttpJsonResponse(resposta);
+            resp->setStatusCode(drogon::k201Created); //res.status(201)
+            __callback(resp);
+    },
+    {drogon::Post}
+);
+
+    std::cout << "Servidor rodando em http://localhost:8080" << std::endl;
+
+    // Equivalente ao "app.listen(8080)" do Express — bloqueia e mantém o servidor vivo
+    drogon::app().addListener("0.0.0.0", 8080).run();
+
+    return 0;
 }
