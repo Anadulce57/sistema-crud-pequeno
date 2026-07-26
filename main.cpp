42d04f084cf4c1227ebb8889ab361f474f008043
Anadulce57 <andl2008@hotmail.com>
2026-07-26 00:04:59 -0300

    Iniciando servidor com drogon na porta 8080

--- main.cpp
+++ main.cpp
@@ -1,6 +1,13 @@
 #include <iostream> //Declarando bibliotecas
-#include <sqlite3.h> //Praticamente o express
-#include <drogon/drogon.h> //Praticamente o npm
+#include <sqlite3.h> //Banco
+#include <drogon/drogon.h> //Praticamente o express - framework
 
 const std::string DB_PATH = "biblioteca.DB";
 
+int main() {
+    std::cout << "Servidor rodando comm sucesso na porta 8080..." << std::endl;
+    //Configurar o servidor pra ouvir na porta 8080
+     drogon::app().addListener("0.0.0.0", 8080);
+    //Rodar o seervidor
+    drogon::app().run();
+}
